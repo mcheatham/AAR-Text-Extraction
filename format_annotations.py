1cd53f9758cfd76cb3e84ae3562f8e2ba1fc94fd
Michelle Cheatham <michelle.cheatham@gmail.com>
2018-04-29 13:23:35 -0400

    Added the text versions of the AARs, the annotations, and some changes to format_annotations

--- format_annotations.py
+++ format_annotations.py
@@ -1,33 +1,57 @@
 import os
 import xlrd
+import pandas
 import re
 
-annotationDir = "AAR annotations"
-reportDir = "AAR documents"
+annotationsDir = "AAR annotations"
+documentsDir = "AAR documents"
 
-#for f in os.listdir(annotationDir):
-#
-#    if f.endswith(".xlsx") and f.startswith("P"):
-#
-#        path = os.path.join(annotationDir, f)
-#        # print(path)
-#        annotationFile = pandas.read_excel(path)
-#
-#        # print(annotationFile.columns)
-#
-#        strengths = annotationFile['Pros/Successful tasks'].values
-#        # print(strengths)
-#
-#        weaknesses = annotationFile['Cons/Recommendations'].values
-#        # print(weaknesses)
-#
-#        # we to output a file with the document id number, 
-#        # and the strengths and weaknesses for that document, 
-#        # separated into individual sentences
-#
-#        # TODO figure out which document each row is for
-#        for i in range(0, len(annotationFile.count())):
-#
-                        #if strengths[i] in page_content or weaknesses[i] in page_content:
+one = False
+
+for f in os.listdir(annotationsDir):
+
+    if f.endswith(".xlsx") and f.startswith("P"):
+
+        one = True
+
+        path = os.path.join(annotationsDir, f)
+        print(path)
+        annotationFile = pandas.read_excel(path)
+
+        # print(annotationFile.columns)
+
+        strengths = annotationFile['Pros/Successful tasks'].values
+        print(strengths)
+
+        weaknesses = annotationFile['Cons/Recommendations'].values
+        # print(weaknesses)
+
+        # we to output a file with the document id number
+        # and the strengths and weaknesses for that document
+
+        # TODO figure out which document each row is for
+        for i in range(0, len(strengths)):
+
+            # look through each of the .txt files in the docs dir
+            found = False
+
+            for doc in os.listdir(documentsDir):
+
+                if doc.endswith(".txt"):
+
+                    docPath = os.path.join(documentsDir, doc)
+                    with open(docPath, 'r') as docFile:
+
+                        docContents = docFile.read()
+
+                        if strengths[i] in docContents or weaknesses[i] in docContents:
+
+                            #print(i, " is ", doc)
                         #    print(j + " " + strengths[i] + " " + weaknesses[i])
-                        #    break
+                            found = True
+                            break
+
+            #if not found:
+                #print(i, " not found in the documents")
+    if one:
+        break
